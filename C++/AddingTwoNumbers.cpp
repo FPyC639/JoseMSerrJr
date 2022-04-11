2188a84d1ef127b8c469ac8ee110dba06302b399
Jose M. Serra Jr <71533268+FPyC639@users.noreply.github.com>
2022-04-11 14:24:25 -0400

    Added Assembly

---
+++ C++/AddingTwoNumbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int number;
+    int number2;
+    cout << "Please insert the first number: ";
+    cin >> number;
+    cout << "\nPlease insert the second number: ";
+    cin >> number2;
+
+    cout << "The summation of the numbers is the following: " << number + number2 << endl;
+
+    return 0;
+
+
+}
