0a32ffb897c4231d87d593e23b36c64c998408b2
Jose M. Serra Jr <joseserra803@gmail.com>
2022-05-09 14:18:53 -0400

    Improved Fibs Sequence.

--- Python/FibsDict.py
+++ Python/FibsDict.py
@@ -3,12 +3,14 @@ b = dict()
 def Fibs(a):
     b[0] = 0
     b[1] = 1
-    for i in range(2,a+1):
+    b[2] = 1
+    for i in range(3,a+1):
         b[i]= b.get(i,b[i-2]) +b[i-1]
     return b[a]
 def Fibs2(a):
     if a==0: return 0
     elif a == 1: return 1
+    elif a == 2: return 1
     else: return Fibs2(a-2) + Fibs2(a-1)
 begin = time.perf_counter()
 print(Fibs(5))
